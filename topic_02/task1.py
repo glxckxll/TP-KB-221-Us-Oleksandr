d30588426fbf65c5a9c7e47fc487b2273a56adbf
sasshko <olexxxandrus@stu.cn.ua>
2023-10-02 21:46:56 +0300

    a little changes

--- topic_02/task1.py
+++ topic_02/task1.py
@@ -1,26 +1,28 @@
 import math
+
+def calculate_discriminant(a, b, c):
+    return b**2 - 4*a*c
+
 def solve_quadratic_equation(a, b, c):
-    # Обчислюємо дискримінант
-    discriminant = b**2 - 4*a*c
-    # Перевіряємо значення дискримінанту
-    if discriminant > 0:
-        # Два корені
-        root1 = (-b + math.sqrt(discriminant)) / (2*a)
-        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+    discriminant_value = calculate_discriminant(a, b, c)
+    # Розрахунок
+    if discriminant_value > 0:
+    # Два корені
+        root1 = (-b + math.sqrt(discriminant_value)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant_value)) / (2*a)
         return root1, root2
-    elif discriminant == 0:
-        # Один корінь 
+    elif discriminant_value == 0:
+    # Один корінь
         root = -b / (2*a)
         return root
     else:
-        # Розв'язків немає 
+        
         return "Розв'язків немає"
-    
-# Введення коефіцієнтів квадратного рівняння від користувача
+
 a = float(input("Введіть коефіцієнт a: "))
 b = float(input("Введіть коефіцієнт b: "))
 c = float(input("Введіть коефіцієнт c: "))
 
-# Виклик функції та виведення результату
+# Виклик функції
 result = solve_quadratic_equation(a, b, c)
 print("Результат:", result)
