841f6d5bd4ec72cf27f58ddd19b67e4020c9d74e
sasshko <olexxxandrus@stu.cn.ua>
2023-10-02 20:23:51 +0300

    a litte report changes

--- topic_02/task2.py
+++ topic_02/task2.py
@@ -1,23 +1,36 @@
-# Введення чисел та операції від користувача
-num1 = float(input("Введіть перше число: "))
-operator = input("Введіть операцію (+, -, *, /): ")
-num2 = float(input("Введіть друге число: "))
+a = float(input("Число a: "))
+b = float(input("Число b: "))
 
-# Виконання операції в залежності від оператора
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    # Перевірка на ділення на нуль
-    if num2 != 0:
-        result = num1 / num2
+operation = input("Операція (+, -, *, /): ")
+    #функції
+def sum(x, y):
+    return x + y
+
+def min(x, y):
+    return x - y
+
+def mult(x, y):
+    return x * y
+
+def div(x, y):
+    if y == 0:
+        return "На нуль не можна ділити"
+    return x / y
+
+    #обчислення
+def calculator(a, b, operation):
+
+    if operation == "+":
+        return sum(a,b)
+    elif operation == "-":
+        return min(a,b)
+    elif operation == "*":
+        return mult(a,b)
+    elif operation == "/":
+        return div(a,b)
     else:
-        result = "Помилка: ділення на нуль"
-else:
-    result = "Невідомий оператор"
+        return "Помилка"
 
-# Виведення результату
-print("Результат:", result)
+result = calculator(a, b, operation)
+
+print(f"Результат: {result}")
--- topic_02/task3.py
+++ topic_02/task3.py
@@ -1,35 +1,38 @@
-while True:
-    print("Welcome to while calc")
-    s = input("Знак (+, -, *, /): ")
+a = float(input("Число a: "))
+b = float(input("Число b: "))
 
-    # Перевірка на допустимий знак операції
-    if s not in ("+", "-", "*", "/"):
-        print("Невірний знак операції!")
-        continue
+operation = (input("Операція (+, -, *, /): "))
+    #функції
+def sum(x, y):
+    return x + y
 
-    try:
-        # Введення чисел
-        a = float(input("a = "))
-        b = float(input("b = "))
+def min(x, y):
+    return x - y
 
-        # Виконання операції та виведення результату
-        match s:
-            case "+":
-                print("%.2f" % (a + b))
-            case "-":
-                print("%.2f" % (a - b))
-            case "*":
-                print("%.2f" % (a * b))
-            case "/":
-                if b != 0:
-                    print("%.2f" % (a / b))
-                else:
-                    print("Ділення на нуль!")
-    except ValueError:
-        print("Введено невірне число. Будь ласка, введіть числа знову.")
-    except ZeroDivisionError:
-        print("Ділення на нуль недопустиме. Будь ласка, введіть b знову.")
-    
-    another_calculation = input("Бажаєте виконати ще одну операцію? (y/n): ")
-    if another_calculation.lower() != "y":
-        break
+def mult(x, y):
+    return x * y
+
+def div(x, y):
+    if y == 0:
+        return "На нуль не можна ділити"
+    return x / y
+
+    #обчислення
+match operation:
+   
+    case "-":
+     print(f"Результат: {min(a, b)}")
+     
+    case "+":
+     print(f"Результат: {sum(a, b)}")
+     
+    case "*":
+     print(f"Результат:{mult(a, b)}")
+      
+    case "/":
+     if b != 0:
+      print(f"Результат: {div(a, b)}")
+     
+     else:
+       print("Помилка")
+
